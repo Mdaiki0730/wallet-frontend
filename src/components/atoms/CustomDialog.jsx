be2d55254663b435827218bb274b50ea76a4c9d0
Daiki Muroga <garian@DaikinoMacBook-Air.local>
2022-06-20 02:13:30 +0900

    prevent multiple button click

--- src/components/atoms/CustomDialog.jsx
+++ src/components/atoms/CustomDialog.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
@@ -6,7 +6,22 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec))
+
 const CustomDialog = ({ open, handleClose, title, content, onClickOK }) => {
+  const [ disabled, setDisabled ] = useState(false);
+  const onClick = () => {
+    setDisabled(true)
+  }
+  useEffect(() => {
+    if (!disabled) return
+    const clickProcess = async () => {
+      await onClickOK()
+      await sleep(2000)
+      setDisabled(false)
+    }
+    clickProcess()
+  }, [disabled])
   return (
     <Dialog open={open} onClose={handleClose}>
       <DialogTitle color="secondary">
@@ -19,7 +34,7 @@ const CustomDialog = ({ open, handleClose, title, content, onClickOK }) => {
       </DialogContent>
       <DialogActions>
         <Button color="secondary" variant="contained" onClick={handleClose}>Cancel</Button>
-        <Button color="primary" variant="contained" onClick={onClickOK}>OK</Button>
+        <Button color="primary" variant="contained" disabled={disabled} onClick={onClick}>OK</Button>
       </DialogActions>
     </Dialog>
   )
